abba9dc3aceb979ad3fc8d37f4254978fc81dda1
rajpatel0909 <rajp.uf16@gmail.com>
2016-11-02 11:52:42 -0400

    all as methods

--- main.py
+++ main.py
@@ -141,17 +141,19 @@ def letor():
     TminusP = testY - predic
     Erms = np.dot(np.transpose(TminusP),TminusP)
     ErmsTest = np.sqrt(Erms/testLen)
-    plt.figure(1)
+    """plt.figure(1)
     plt.plot(graphX,predic,'r--', graphX, testY, 'b--')
     plt.xlabel("data points")
-    plt.ylabel("values")
+    plt.ylabel("Target and Predicted values")
     plt.title("LeToR Closed Form Solution")
     plt.show()
 
     fig = plt.figure(2)    
     plt.imshow(contourMatrix, interpolation='nearest', cmap=plt.cm.ocean)
     plt.colorbar()
-    plt.show()
+    plt.show()"""
+    
+
     #stochastic gradient descent  #0.3
     eta = 1
     mSh = validMinValues['m']
@@ -165,7 +167,7 @@ def letor():
         wSh += (tempW/trainLen)
         cost1 = trainY - np.dot(phiTrain,wSh)
         cost2 = np.dot(np.transpose(cost1),cost1)
-        costValues.append(cost2/trainLen)
+        costValues.append(np.sqrt(cost2/trainLen))
     
     phiWSh = np.dot(phiTrain,wSh)
     TminusPSh = trainY - phiWSh    
@@ -182,14 +184,14 @@ def letor():
     ErmsSh = np.dot(np.transpose(TminusPSh),TminusPSh)
     ErmsShTest = np.sqrt(ErmsSh/testLen)
     
-    
+    """
     predic = np.dot(phiTest,wSh)
     graphX = list(range(testLen))
     
     plt.figure(3)
     plt.plot(graphX,predic,'r--', graphX, testY, 'b--')
     plt.xlabel("data points")
-    plt.ylabel("values")
+    plt.ylabel("Target and Predicted values")
     plt.title("LeToR Stochastic Gradient Descent")
     plt.show()
     
@@ -199,21 +201,12 @@ def letor():
     plt.xlabel("iterations")
     plt.ylabel("cost")
     plt.title("LeToR Cost Function for eta = 1")
-    plt.show()
+    plt.show()"""
     
     
     
 
-#def synthetic():
-    
-    
-    
-if __name__ == "__main__":
-    print "Hello World"
-    #letor()
-    
-    
-    #synthetic()m = 56 eta = 0.03"""
+def synthetic():
     X = np.matrix
     Y = np.matrix
     inFile = open("input.csv", 'rU')
@@ -240,20 +233,20 @@ if __name__ == "__main__":
     validLen = int((length - trainLen)/2)
     testLen = length - trainLen - validLen
     
-    """randNums = np.random.choice(length, size = length, replace = False)
+    randNums = np.random.choice(length, size = length, replace = False)
     trainX = X[randNums[0:trainLen]]
     trainY = Y[randNums[0:trainLen]]
     validX = X[randNums[trainLen:trainLen + validLen]]
     validY = Y[randNums[trainLen:trainLen + validLen]]
     testX = X[randNums[trainLen + validLen:]]
-    testY = Y[randNums[trainLen + validLen:]]"""
+    testY = Y[randNums[trainLen + validLen:]]
     
-    trainX = X[0:trainLen]
+    """trainX = X[0:trainLen]
     trainY = Y[0:trainLen]
     validX = X[trainLen:trainLen + validLen]
     validY = Y[trainLen:trainLen + validLen]
     testX = X[trainLen + validLen:]
-    testY = Y[trainLen + validLen:]
+    testY = Y[trainLen + validLen:]"""
     
     
     var = []
@@ -294,7 +287,7 @@ if __name__ == "__main__":
                 XminusMu = np.subtract(validX[i],mu[j-1])
                 phiValid[i][j]= np.exp(-0.5*(np.dot(np.dot(XminusMu,sigmaInv),(np.transpose(XminusMu)))))
                 
-        lamb = 0.00
+        lamb = 0.01
         
         while(lamb < 0.02):
             eye = np.eye(m, k = lamb)
@@ -348,7 +341,8 @@ if __name__ == "__main__":
     TminusP = testY - predic
     Erms = np.dot(np.transpose(TminusP),TminusP)
     ErmsTest = np.sqrt(Erms/testLen)
-    plt.figure(1)
+    
+    """plt.figure(1)
     plt.plot(graphX,predic,'r--', graphX, testY, 'b--')
     plt.xlabel("data points")
     plt.ylabel("Target and Predicted values")
@@ -358,31 +352,43 @@ if __name__ == "__main__":
     fig = plt.figure(2)    
     plt.imshow(contourMatrix, interpolation='nearest', cmap=plt.cm.ocean)
     plt.colorbar()
-    plt.show()
-    #stochastic gradient descent  #0.3
-    eta = 0.01
-    mSh = validMinValues['m']
-    costValues = []
-    wSh = np.ones((mSh,1))
-    iterations = 50
-    for i in range(0,iterations):
-        eyeSh = np.eye(mSh, k = validMinValues['lamb'])
-        lambWSh = np.transpose(np.dot(eyeSh,wSh))
-        tempW = np.transpose(-eta*np.add((-1*np.dot(np.transpose(trainY-np.dot(phiTrain,wSh)),phiTrain)),lambWSh))
-        wSh += (tempW/trainLen)
-        cost1 = trainY - np.dot(phiTrain,wSh)
-        cost2 = np.dot(np.transpose(cost1),cost1)
-        costValues.append(cost2/trainLen)
+    plt.show()"""
     
-    phiWSh = np.dot(phiTrain,wSh)
-    TminusPSh = trainY - phiWSh    
-    ErmsSh = np.dot(np.transpose(TminusPSh),TminusPSh)
-    ErmsShTrain = np.sqrt(ErmsSh/trainLen)
-    
-    phiWSh = np.dot(phiValid,wSh)
-    TminusPSh = validY - phiWSh    
-    ErmsSh = np.dot(np.transpose(TminusPSh),TminusPSh)
-    ErmsShValid = np.sqrt(ErmsSh/validLen)
+
+    #stochastic gradient descent  
+    Eglobal = float("Inf")
+    minEta = 0
+    eta = 0.27
+    while(eta < 0.3):
+        mSh = validMinValues['m']
+        costValues = []
+        wSh = np.ones((mSh,1))
+        iterations = 50
+        for i in range(0,iterations):
+            eyeSh = np.eye(mSh, k = validMinValues['lamb'])
+            lambWSh = np.transpose(np.dot(eyeSh,wSh))
+            tempW = np.transpose(-eta*np.add((-1*np.dot(np.transpose(trainY-np.dot(phiTrain,wSh)),phiTrain)),lambWSh))
+            wSh += (tempW/trainLen)
+            cost1 = trainY - np.dot(phiTrain,wSh)
+            cost2 = np.dot(np.transpose(cost1),cost1)
+            costValues.append(np.sqrt(cost2/trainLen))
+        
+        phiWSh = np.dot(phiTrain,wSh)
+        TminusPSh = trainY - phiWSh    
+        ErmsSh = np.dot(np.transpose(TminusPSh),TminusPSh)
+        ErmsShTrain = np.sqrt(ErmsSh/trainLen)
+        
+        phiWSh = np.dot(phiValid,wSh)
+        TminusPSh = validY - phiWSh    
+        ErmsSh = np.dot(np.transpose(TminusPSh),TminusPSh)
+        ErmsShValid = np.sqrt(ErmsSh/validLen)
+        
+        if(Eglobal > ErmsShValid):
+            Eglobal = ErmsShValid
+            minEta = eta
+        
+        print eta
+        eta = eta*3
     
     phiWSh = np.dot(phiTest,wSh)
     TminusPSh = testY - phiWSh    
@@ -390,7 +396,8 @@ if __name__ == "__main__":
     ErmsShTest = np.sqrt(ErmsSh/testLen)
     
     
-    predic = np.dot(phiTest,wSh)
+    # to plot graph remove comments below
+    """predic = np.dot(phiTest,wSh)
     graphX = list(range(testLen))
     
     plt.figure(3)
@@ -405,8 +412,17 @@ if __name__ == "__main__":
     plt.scatter(graphCostX, costValues)
     plt.xlabel("iterations")
     plt.ylabel("cost")
-    plt.title("Synthetic Cost Function for eta = 0.003")
-    plt.show()
-        
+    plt.title("Synthetic Cost Function for eta = 0.01")
+    plt.show()"""
+            
+    
+    
+if __name__ == "__main__":
+    #print "Hello World"
+    letor()
+    
+    
+    synthetic()
+    
     
     
